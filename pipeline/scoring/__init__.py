bfd7e79c41c169674c549e403dbafb7e5af1fbff
Leandro Silva <leandroantoniooliveirasilva@gmail.com>
2026-04-18 23:43:09 +0100

    chore: run pipeline and fix WEIGHTS import rename

--- pipeline/scoring/__init__.py
+++ pipeline/scoring/__init__.py
@@ -1,11 +1,12 @@
 # Scoring package
-from .composite import compute_composite, WEIGHTS
+from .composite import compute_composite, WEIGHTS_BY_TYPE, get_weights
 from .rsi import compute_rsi
 from .actions import derive_action
 
 __all__ = [
     "compute_composite",
-    "WEIGHTS",
+    "WEIGHTS_BY_TYPE",
+    "get_weights",
     "compute_rsi",
     "derive_action",
 ]
